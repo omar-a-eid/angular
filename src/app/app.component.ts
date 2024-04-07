033bd100132b82e4387c59599eb295bd80d7b646
omar-a-eid <omerx9988@gmail.com>
2024-04-07 13:07:08 +0200

    day2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { InputComponent } from './components/input/input.component';
+import { SliderComponent } from './components/slider/slider.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [InputComponent, SliderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'demo';
---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css',
+})
+export class InputComponent {
+  name: string = '';
+  reset() {
+    this.name = '';
+  }
+}
